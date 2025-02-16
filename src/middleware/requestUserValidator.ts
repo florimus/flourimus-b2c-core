dd2cfcd9bb01155db438a4d9efa97aa852a643df
Anoopss <anoopviswanadh007@gmail.com>
2025-02-16 21:04:27 +0530

    feat: doc added

--- src/middleware/requestUserValidator.ts
+++ src/middleware/requestUserValidator.ts
@@ -4,8 +4,24 @@ import UnAuthorized from '@src/errorBoundary/custom/unauthorized.error';
 import { User } from '@core/types';
 import userService from '@service/user.service';
 
+/**
+ * Middleware to validate the user from the request.
+ * 
+ * This middleware checks if the email is present in the response locals.
+ * If the email is not found, it logs an error and throws an UnAuthorized error.
+ * It then fetches the user information using the email.
+ * If the user is not found, it logs an error and throws an UnAuthorized error.
+ * If the user is found, it sets the user's role and _id in the response locals.
+ * Finally, it calls the next middleware in the stack.
+ * 
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next middleware function.
+ * 
+ * @throws {UnAuthorized} If the email is not found in the token or the user does not exist.
+ */
 const requestUserValidator = async (
-  req: Request,
+  _req: Request,
   res: Response,
   next: NextFunction
 ) => {
--- src/middleware/tokenValidator.ts
+++ src/middleware/tokenValidator.ts
@@ -4,6 +4,21 @@ import Logger from '@core/logger';
 import UnAuthorized from '@src/errorBoundary/custom/unauthorized.error';
 import { validateToken } from '@core/utils/jwt.utils';
 
+/**
+ * Middleware to validate the JWT token from the request headers.
+ * 
+ * This middleware checks for the presence of an authorization header
+ * and verifies that it contains a valid JWT token. If the token is valid,
+ * it extracts the email from the token payload and attaches it to the 
+ * response locals. If the token is invalid or missing, it throws an 
+ * UnAuthorized error.
+ * 
+ * @param req - The incoming request object.
+ * @param res - The outgoing response object.
+ * @param next - The next middleware function in the stack.
+ * 
+ * @throws {UnAuthorized} If the authorization header is missing or the token is invalid.
+ */
 const tokenValidator = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
