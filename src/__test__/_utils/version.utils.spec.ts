3d56a6e4a9cdbed7dcb083e8484e9f01d650a77c
Anoopss <anoopviswanadh007@gmail.com>
2025-02-19 21:53:09 +0530

    test: testcase added

--- src/__test__/_utils/version.utils.spec.ts
+++ src/__test__/_utils/version.utils.spec.ts
@@ -2,22 +2,22 @@ import versionControl from '@core/utils/version.utils';
 
 describe('versionControl', () => {
     it('should increment the version number by 1 when a version is provided', () => {
-        const result = versionControl(1);
+        const result = versionControl('admin@hgmail.com', 1);
         expect(result.version).toBe(2);
     });
 
     it('should return version 1 when no version is provided', () => {
-        const result = versionControl();
+        const result = versionControl('admin@hgmail.com');
         expect(result.version).toBe(1);
     });
 
     it('should increment the version number by 1 when a version greater than 1 is provided', () => {
-        const result = versionControl(5);
+        const result = versionControl('admin@hgmail.com', 5);
         expect(result.version).toBe(6);
     });
 
     it('should handle zero as a version number correctly', () => {
-        const result = versionControl(0);
+        const result = versionControl('admin@hgmail.com',0);
         expect(result.version).toBe(1);
     });
 });
